10bbf1d9e8729cde152207b26cc39453bc9e538c
jramirez <jramirez007>
2023-01-03 11:36:49 -0600

    Implementando deducciones de empleador

--- resources/views/job_request/div_deductions_medical.blade.php
+++ resources/views/job_request/div_deductions_medical.blade.php
@@ -1,102 +1,113 @@
 @if ($ChkMedical == 1)
-<div class="col-sm-12">
-    <div class="card-header">
-        <h4 class="card-title">{!! trans('job_application.DailyTransportationTitle') !!}</h4>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.DeductionPerWeek') !!}</label>
-                <input type="number" step="0.01" name="deduction_daily_amount_person_week" required class="form-control">
+    <div class="col-sm-12">
+        <div class="card-header">
+            <h4 class="card-title"></h4>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.DeductionAmountMedical') !!}</label>
+                    <input type="number" step="0.01" name="deduction_daily_amount_person_week" required
+                        class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.PlansPremium') !!}</label>
+                </div>
+
             </div>
 
-        </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
+                    <input type="text" name="daily_notes" class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.additional_notes_medical') !!}</label>
+                </div>
 
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
-                <input type="text" name="daily_notes" class="form-control">
             </div>
-
         </div>
     </div>
-</div>
-
 @endif
 
 @if ($ChkDental == 1)
-<div class="col-sm-12">
-    <div class="card-header">
-        <h4 class="card-title">{!! trans('job_application.DailyTransportationTitle') !!}</h4>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.DeductionPerWeek') !!}</label>
-                <input type="number" step="0.01" name="deduction_daily_amount_person_week" required class="form-control">
+    <div class="col-sm-12">
+        <div class="card-header">
+            <h4 class="card-title"></h4>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.DeductionAmountDental') !!}</label>
+                    <input type="number" step="0.01" name="deduction_daily_amount_person_week" required
+                        class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.PlansPremium') !!}</label>
+                </div>
+
             </div>
 
-        </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
+                    <input type="text" name="daily_notes" class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.additional_notes_medical') !!}</label>
+                </div>
 
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
-                <input type="text" name="daily_notes" class="form-control">
             </div>
-
         </div>
     </div>
-</div>
 @endif
 
 
 @if ($ChkVision == 1)
-<div class="col-sm-12">
-    <div class="card-header">
-        <h4 class="card-title">{!! trans('job_application.DailyTransportationTitle') !!}</h4>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.DeductionPerWeek') !!}</label>
-                <input type="number" step="0.01" name="deduction_daily_amount_person_week" required class="form-control">
+    <div class="col-sm-12">
+        <div class="card-header">
+            <h4 class="card-title"></h4>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.DeductionAmountVision') !!}</label>
+                    <input type="number" step="0.01" name="deduction_daily_amount_person_week" required
+                        class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.PlansPremium') !!}</label>
+                </div>
+
             </div>
 
-        </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
+                    <input type="text" name="daily_notes" class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.additional_notes_medical') !!}</label>
+                </div>
 
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
-                <input type="text" name="daily_notes" class="form-control">
             </div>
-
         </div>
     </div>
-</div>
 @endif
 
 
 @if ($ChkOther == 1)
-<div class="col-sm-12">
-    <div class="card-header">
-        <h4 class="card-title">{!! trans('job_application.DailyTransportationTitle') !!}</h4>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.DeductionPerWeek') !!}</label>
-                <input type="number" step="0.01" name="deduction_daily_amount_person_week" required class="form-control">
+    <div class="col-sm-12">
+        <div class="card-header">
+            <h4 class="card-title"></h4>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.DeductionAmountOther') !!}</label>
+                    <input type="number" step="0.01" name="deduction_daily_amount_person_week" required
+                        class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.PlansPremium') !!}</label>
+                </div>
+
             </div>
 
-        </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
+                    <input type="text" name="daily_notes" class="form-control">
+                    <label for="exampleInputEmail1">{!! trans('job_application.additional_notes_medical') !!}</label>
+                </div>
 
-        <div class="col-md-6">
-            <div class="form-group">
-                <label for="exampleInputEmail1">{!! trans('job_application.notes') !!}</label>
-                <input type="text" name="daily_notes" class="form-control">
             </div>
-
         </div>
     </div>
-</div>
 @endif
